0c0fd92459742cad3fa115c7d9554c3e622bd9cb
Le Ngoc Quang <126934622+lengocquanggit255@users.noreply.github.com>
2024-10-12 10:37:04 +0700

    weather css

--- src/app/code/Flshop/Weather/Block/Weather.php
+++ src/app/code/Flshop/Weather/Block/Weather.php
@@ -5,25 +5,50 @@ namespace Flshop\Weather\Block;
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\HTTP\Client\Curl;
 use Psr\Log\LoggerInterface;
+use Magento\Framework\App\CacheInterface as CacheManager;
+use Magento\Framework\Serialize\SerializerInterface;
 
 class Weather extends Template
 {
     protected $curl;
     protected $logger;
+    protected $cacheManager;
+    protected $serializer;
+
+    const CACHE_TAG = 'flshop_weather_data';
+    const CACHE_LIFETIME = 600; // 10 minutes
 
     public function __construct(
         Template\Context $context,
         Curl $curl,
         LoggerInterface $logger,
+        CacheManager $cacheManager,
+        SerializerInterface $serializer,
         array $data = []
     ) {
         $this->curl = $curl;
         $this->logger = $logger;
+        $this->cacheManager = $cacheManager;
+        $this->serializer = $serializer;
         parent::__construct($context, $data);
     }
 
+    /**
+     * Fetch weather data for a single city with caching
+     *
+     * @param string $city
+     * @return array
+     */
     public function getWeatherData($city = 'HaNoi')
     {
+        $cacheKey = self::CACHE_TAG . '_' . strtolower(str_replace(' ', '_', $city));
+        $cachedData = $this->cacheManager->load($cacheKey);
+
+        if ($cachedData) {
+            $weatherData = $this->serializer->unserialize($cachedData);
+            return $weatherData;
+        }
+
         $apiKey = '7a20a3a7199507e141cf626aa977dbdf'; // Replace with your OpenWeatherMap API key
         $apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' . urlencode($city) . '&units=metric&appid=' . $apiKey;
 
@@ -42,6 +67,9 @@ class Weather extends Template
             }
 
             if (isset($weatherData['cod']) && $weatherData['cod'] == 200) {
+                // Serialize and save to cache
+                $serializedData = $this->serializer->serialize($weatherData);
+                $this->cacheManager->save($serializedData, $cacheKey, [self::CACHE_TAG], self::CACHE_LIFETIME);
                 return $weatherData;
             } else {
                 $errorMessage = isset($weatherData['message']) ? $weatherData['message'] : 'Unknown error';
@@ -50,12 +78,28 @@ class Weather extends Template
         } catch (\Exception $e) {
             // Log the error message
             $this->logger->error('Weather API Error: ' . $e->getMessage());
-            // Optionally, display a generic error message to the user
-            echo '<p>Sorry, we are unable to display weather information at this time.</p>';
+            // Optionally, return cached data if available
+            return [];
+        }
+    }
+
+    /**
+     * Fetch weather data for multiple cities with caching
+     *
+     * @param array $cities
+     * @return array
+     */
+    public function getMultipleWeatherData(array $cities)
+    {
+        $allWeatherData = [];
+
+        foreach ($cities as $city) {
+            $data = $this->getWeatherData($city);
+            if (!empty($data)) {
+                $allWeatherData[] = $data;
+            }
         }
 
-        return [];
+        return $allWeatherData;
     }
 }
-https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&appid=7a20a3a7199507e141cf626aa977dbdf
-
